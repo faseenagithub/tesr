7bccbfc5e5c21e0322ce4da82e7cab4cd7985cc8
Favas K <favas.k@fingent.com>
2023-01-25 18:49:22 +0530

    Initial commit

--- routes/web.php
+++ routes/web.php
@@ -13,21 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-    Route::get('/', function () { return view('home'); });
+Route::get('/', function () {
+    return view('home');
+});
 
-    //routes for student CRUD
-    Route::get('students-list', 'StudentController@index');
-    Route::post('add-user', 'StudentController@store');
-    Route::get('edit-student/{id}', 'StudentController@edit');
-    Route::post('update-student/{id}', 'StudentController@update');
-    Route::get('delete-student/{id}', 'StudentController@destroy');
-
-    //routes for Marks CRUD
-    Route::get('mark-list','MarkController@index');
-    Route::post('add-mark', 'MarkController@store');
-    Route::get('edit-mark/{id}', 'MarkController@edit');
-    Route::post('update-mark/{id}', 'MarkController@update');
-    Route::get('delete-mark/{id}', 'MarkController@destroy');
-
-
-
+//routes for student CRUD
+Route::get('students-list', 'StudentController@index');
+Route::post('add-user', 'StudentController@store');
+Route::get('edit-student/{id}', 'StudentController@edit');
+Route::post('update-student/{id}', 'StudentController@update');
+Route::get('delete-student/{id}', 'StudentController@destroy');
